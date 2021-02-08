72b855b57b07e817448c88b183423d6422fcc0a0
musta10 <mustafaenfadal@gmail.com>
2021-02-08 16:42:33 +0100

    portada

--- src/components/SliderCard.jsx
+++ src/components/SliderCard.jsx
@@ -4,7 +4,9 @@ import '../css/slidercard.css'
 class SliderCard extends Component {
     render() {
         return(
-            <div></div>
+            <div id="slider" className="slider-principal">
+                <h2>Bienvenido a Trap Family <img src="https://img.icons8.com/emoji/48/000000/hundred-points.png"/> Drill y Trap</h2>
+            </div>
         )
     }
 }
