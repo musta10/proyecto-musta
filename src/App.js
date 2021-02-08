76d124424d42554629f070de25c73831ee3cd9b2
musta10 <mustafaenfadal@gmail.com>
2021-02-08 16:17:00 +0100

    card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/Header'
+import SliderCard from './components/SliderCard'
 function App() {
   return (
     <div className="App">
       <Header />
+      <SliderCard />
       <header className="App-header">
       </header>
     </div>
---
+++ src/components/SliderCard.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import '../css/slidercard.css'
+
+class SliderCard extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default SliderCard;
