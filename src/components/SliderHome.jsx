d492c2bb031572cfb733f10312b82ddfa57f294a
musta10 <mustafaenfadal@gmail.com>
2021-02-08 17:44:47 +0100

    portada-home

---
+++ src/components/SliderHome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import '../css/slidercard.css'
+
+class SliderHome extends Component {
+    render() {
+        return(
+            <div id="slider" className="slider-inicio">
+                <h2>Bienvenido a Trap Family <img src="https://img.icons8.com/emoji/48/000000/hundred-points.png"/> Drill y Trap</h2>
+            </div>
+        )
+    }
+}
+
+export default SliderHome;
