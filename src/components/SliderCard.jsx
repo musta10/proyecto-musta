43ab3db1af921fe2d3b2a85bec55dbfb71da2128
musta10 <mustafaenfadal@gmail.com>
2021-02-08 17:23:40 +0100

    paginas

--- src/components/SliderCard.jsx
+++ src/components/SliderCard.jsx
@@ -4,7 +4,7 @@ import '../css/slidercard.css'
 class SliderCard extends Component {
     render() {
         return(
-            <div id="slider" className="slider-principal">
+            <div id="slider" className="slider-paginas">
                 <h2>Bienvenido a Trap Family <img src="https://img.icons8.com/emoji/48/000000/hundred-points.png"/> Drill y Trap</h2>
                 <button className="btn-home">Ir a Inicio</button>
             </div>
