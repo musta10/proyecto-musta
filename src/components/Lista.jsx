10abe2801d11cb48f24afa2b72bcbdf8bf2414c5
musta10 <mustafaenfadal@gmail.com>
2021-02-10 21:49:48 +0100

    categorias

---
+++ src/components/Lista.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Lista extends Component {
+    render() {
+        return(
+          <div>lista categoria</div>
+        )
+    }
+}
+
+export default Lista;
