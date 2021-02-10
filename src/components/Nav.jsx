36a366d179140e9e1abe2a078a25e0d7106077d7
musta10 <mustafaenfadal@gmail.com>
2021-02-11 00:55:21 +0100

    componentstyle

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
 import logo from "../assets/logo/palmeracirculo.png";
-import Lista from './Lista'
-import '../css/header.css'
+import Lista from "./Lista";
+import "../css/header.css";
+import styled from "styled-components";
 
 class Nav extends Component {
-    render() {
-        return(
-          <>
-          <nav>
+  render() {
+    return (
+      <>
+        <nav>
           {/* EL LOGO */}
           <div id="logo">
-            <img
-              src={logo}
-              alt="Logotipo"
-              width="40"
-              height="40"
-            />
+            <img src={logo} alt="Logotipo" width="40" height="40" />
             <span id="brand">
               <strong>Trap</strong>Family
             </span>
           </div>
-        <div className="titulo">
-          <p>Musica <strong>Trap</strong>  y Drill</p>
-        </div>
-            </nav>
-            {/* <Lista /> */}
-            </>
-        )
-    }
+          <div className="titulo">
+            <p>
+              Musica <strong>Trap</strong> y Drill
+            </p>
+          </div>
+        </nav>
+        {/* <Lista /> */}
+      </>
+    );
+  }
 }
 
 export default Nav;
