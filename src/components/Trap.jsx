8927571f9eb6c191a84eb96a3773a95fc0fa756a
musta10 <mustafaenfadal@gmail.com>
2021-02-12 01:05:31 +0100

    musica

--- src/components/Trap.jsx
+++ src/components/Trap.jsx
@@ -1,42 +1,84 @@
 import React, { Component } from "react";
-import '../css/trap.css'
-import ListGroup from 'react-bootstrap/ListGroup'
+import "../css/trap.css";
+// import ListGroup from "react-bootstrap/ListGroup";
 class Trap extends Component {
   render() {
     return (
-        <>
-        <ListGroup>
+      <>
+        {/* <ListGroup>
+  <ListGroup.Item><a href="https://youtube.com/c/MykeTowers">Myke Towers</a></ListGroup.Item>
+  <ListGroup.Item><a href="https://www.youtube.com/c/BadBunnyPR">Bad Bunny</a></ListGroup.Item>
+  <ListGroup.Item><a href="https://www.youtube.com/user/BRYTIAGOTV">Brytiago</a></ListGroup.Item>
+  <ListGroup.Item><a href="https://youtube.com/c/AnuelAA">AnuelAA</a></ListGroup.Item>
+</ListGroup> */}
         <h3 className="titulo">Quien es el Rey del Trap ?</h3>
         <p className="titulo">21 artistas cantan Trap</p>
-  <ListGroup.Item><a href="">Myke Towers</a></ListGroup.Item>
-  <ListGroup.Item><a href="">Bad Bunny</a></ListGroup.Item>
-  <ListGroup.Item><a href="">Brytiago</a></ListGroup.Item>
-  <ListGroup.Item><a href="">AnuelAA</a></ListGroup.Item>
-</ListGroup>
-    {/* <ul className="menu">
-    <li><a href="">Myke Towers</a></li>
-    <li><a href="">Bad Bunny</a></li>
-    <li><a href="">Brytiago</a></li>
-    <li><a href="">AnuelAA</a></li>
-    <li><a href="">Bryant Myers</a></li>
-    <li><a href="">Jhay Cortez</a></li>
-    <li><a href="">Ñengo Flow</a></li>
-    <li><a href="">Miky Woodz</a></li>
-    <li><a href="">Arcangel</a></li>
-    <li><a href="">Eladio Carrion</a></li>
-    <li><a href="">Kendo Kaponi </a></li>
-    <li><a href="">Almighty</a></li>
-    <li><a href="">Darell</a></li>
-    <li><a href="">jonZ</a></li>
-    <li><a href="">Noriel</a></li>
-    <li><a href="">Cosculluela</a></li>
-    <li><a href="">Lary Over</a></li>
-    <li><a href="">Ele A El Dominio</a></li>
-    <li><a href="">Ñejo</a></li>
-    <li><a href="">Jamby El Favo</a></li>
-    <li><a href="">Anonimus</a></li>
-</ul> */}
-</>
+        <ul className="menu">
+          <li>
+            <a href="https://youtube.com/c/MykeTowers">Myke Towers</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/c/BadBunnyPR">Bad Bunny</a>
+          </li>
+          <li>
+            <a href="https://youtube.com/c/AnuelAA">AnuelAA</a>
+          </li>
+          <li>
+            <a href="">Bryant Myers</a>
+          </li>
+          <li>
+            <a href="">Jhay Cortez</a>
+          </li>
+          <li>
+            <a href="">Ñengo Flow</a>
+          </li>
+          <li>
+            <a href="">Miky Woodz</a>
+          </li>
+          <li>
+            <a href="">Arcangel</a>
+          </li>
+          <li>
+            <a href="">Eladio Carrion</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=Rm__S6_ifRQ">Jamby El Favo</a>
+          </li>
+          <li>
+            <a href="">Kendo Kaponi </a>
+          </li>
+          <li>
+            <a href="">Almighty</a>
+          </li>
+          <li>
+            <a href="">Darell</a>
+          </li>
+          <li>
+            <a href="">jonZ</a>
+          </li>
+          <li>
+            <a href="">Noriel</a>
+          </li>
+          <li>
+            <a href="">Cosculluela</a>
+          </li>
+          <li>
+            <a href="">Lary Over</a>
+          </li>
+          <li>
+            <a href="">Ele A El Dominio</a>
+          </li>
+          <li>
+            <a href="">Ñejo</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/user/BRYTIAGOTV">Brytiago</a>
+          </li>
+          <li>
+            <a href="">Anonimus</a>
+          </li>
+        </ul>
+      </>
     );
   }
 }
