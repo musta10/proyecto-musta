4b25a20de79cdf90be1bc7f2bac942ce72e6c529
musta10 <mustafaenfadal@gmail.com>
2021-03-12 20:35:57 +0100

    listas

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -8,100 +8,100 @@ class Lista extends Component {
            <p className="texto-titulo">32 artistas cantan Reggaeton</p>
         <ul className="lista-menu">
           <li>
-            <a href="https://youtube.com/c/MykeTowers">Daddy Yankee</a>
+            <a href="">Daddy Yankee</a>
           </li>
           <li>
-            <a href="https://www.youtube.com/c/BadBunnyPR">Don Omar</a>
+            <a href="">Don Omar</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnuelAA">Tego Calderon</a>
+            <a href="">Tego Calderon</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/BryantMyersTV">Zion y Lennox</a>
+            <a href="">Zion y Lennox</a>
           </li>
           <li>
-            <a href="https://youtube.com/channel/UCvpEyEBML6z-PJJ9XrqgpKQ">Wisin y Yandel</a>
+            <a href="">Wisin y Yandel</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/nengoflow">Ozuna</a>
+            <a href="">Ozuna</a>
           </li>
           <li>
-            <a href="https://www.youtube.com/watch?v=GPKQk_Vg1Qc">Ñejo</a>
+            <a href="">Ñejo</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/MikyWoodzTV">Darkiel</a>
+            <a href="">Darkiel</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/Arcangel">Sech</a>
+            <a href="">Sech</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">J Balvin</a>
+            <a href="">J Balvin</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Nicky Jam</a>
+            <a href="">Nicky Jam</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/EladioCarrion">Feid</a>
+            <a href="">Feid</a>
           </li>
           <li>
-            <a href="https://www.youtube.com/watch?v=Rm__S6_ifRQ">Lunay</a>
+            <a href="">Lunay</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/KendoKaponiTV">Karol G</a>
+            <a href="">Karol G</a>
           </li>
           <li>
-            <a href="https://youtube.com/channel/UC1Favl8RzwbKEJN_D1oryRg">Natti Natasha</a>
+            <a href="">Natti Natasha</a>
           </li>
           <li>
-            <a href="https://youtube.com/channel/UC7i1scMCtNTreXnYs2Sr9Wg">Rosalía</a>
+            <a href="">Rosalía</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/jonztv">Nio Garcia</a>
+            <a href="">Nio Garcia</a>
           </li>
           <li>
-            <a href="https://www.youtube.com/watch?v=3niVHCdB1wA">El Alfa</a>
+            <a href="">El Alfa</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/NorielOfficial">Casper</a>
+            <a href="">Casper</a>
           </li>
           <li>
-            <a href="https://youtu.be/CMvW-1a8ytI">Alexis y Fido</a>
+            <a href="">Alexis y Fido</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/LaryOver">Becky G</a>
+            <a href="">Becky G</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/eLeAmusicTVOficial">Manuel Turizo</a>
+            <a href="">Manuel Turizo</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/nejoelbroko">Maluma</a>
+            <a href="">Maluma</a>
           </li>
           <li>
-            <a href="https://www.youtube.com/user/BRYTIAGOTV">Rauw Alejandro</a>
+            <a href="">Rauw Alejandro</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Kevin Roldan</a>
+            <a href="">Kevin Roldan</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Dalex</a>
+            <a href="">Dalex</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Lenny Tavarez</a>
+            <a href="">Lenny Tavarez</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Justin Quiles</a>
+            <a href="">Justin Quiles</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Kevin Roldan</a>
+            <a href="">Kevin Roldan</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Farruko</a>
+            <a href="">Farruko</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">Gotay El Autentiko</a>
+            <a href="">Gotay El Autentiko</a>
           </li>
           <li>
-            <a href="https://youtube.com/c/AnonimusTV">De La Ghetto</a>
+            <a href="">De La Ghetto</a>
           </li>
         </ul>
       </>
