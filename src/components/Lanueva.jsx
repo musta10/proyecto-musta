a5fe18a88755221f4a14fdd3a1e4f461f79ef0a7
musta10 <mustafaenfadal@gmail.com>
2021-03-12 22:02:04 +0100

    stylescolors

--- src/components/Lanueva.jsx
+++ src/components/Lanueva.jsx
@@ -11,60 +11,71 @@ class Lanueva extends Component {
             <a href="https://youtu.be/AEsLZAO1EDU">Maikel Delacalle</a>
           </li>
           <li>
+            <a href="https://www.youtube.com/watch?v=xMO6fDfscY0">Nickzzy</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=-FaMSxpiTII">El Taiger</a>
+          </li>
+          <li>
+            <a href="https://youtu.be/y8GDCEw8Uj4">El Sulttan</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=mJxZbKgOyt0">Morad</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=j3Lp4M9K-uw">Mora</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/channel/UCFkew7OWYW2AViFz22dy4Og">
+              Beny Junior
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/channel/UC6zfdak7llP8sYkJCt6LAQQ">
+              Aiman JR
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=Hg5YhBhaffk">ThePoing</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/channel/UC8ebER2BnZCsocCWJc47RUA">
+              El Boza
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=16nZ6K7sim4">
+              Don Miguelo
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=OKxHUGs0-Zk">Kd One</a>
+          </li>
+
+          <li>
             <a href="https://youtube.com/channel/UC6ZwHeY5VpBuYmo5TXrO3Ew">
               Juanfran
             </a>
           </li>
           <li>
-            <a href="">El Taiger</a>
+            <a href="https://youtube.com/c/JayWheeler">Jay Wheeler</a>
           </li>
           <li>
-            <a href="">El Sulttan</a>
+            <a href="https://www.youtube.com/watch?v=cLrpiAGeLcc">Paris Boy</a>
           </li>
           <li>
-            <a href="">Morad</a>
+            <a href="https://www.youtube.com/watch?v=f-QknxTsIjw">Bandaga</a>
           </li>
           <li>
-            <a href="">Mora</a>
+            <a href="https://www.youtube.com/channel/UCYopTA1SG3KGd8riW4aQj5g">
+              Hozwal
+            </a>
           </li>
           <li>
-            <a href="">Beny Junior</a>
+            <a href="https://www.youtube.com/watch?v=0czOYSFxEe4">Omy De Oro</a>
           </li>
           <li>
-            <a href="">Aiman JR</a>
-          </li>
-          <li>
-            <a href="">El Boza</a>
-          </li>
-          <li>
-            <a href="">Don Miguelo</a>
-          </li>
-          <li>
-            <a href="">Kd One</a>
-          </li>
-          <li>
-            <a href="">ThePoing</a>
-          </li>
-          <li>
-            <a href="">Jay Wheeler</a>
-          </li>
-          <li>
-            <a href="">Paris Boy</a>
-          </li>
-          <li>
-            <a href="">Bandaga</a>
-          </li>
-          <li>
-            <a href="">Nickzzy</a>
-          </li>
-          <li>
-            <a href="">Hozwal</a>
-          </li>
-          <li>
-            <a href="">Omy De Oro</a>
-          </li>
-          <li>
-            <a href="">RVFV</a>
+            <a href="https://www.youtube.com/watch?v=0ZLw3Insx3A">RVFV</a>
           </li>
         </ul>
       </>
